44a094c45e7651c9fe41d002c6355187e669c6df
ChristopherOng4 <99162371+ChristopherOng4@users.noreply.github.com>
2022-04-11 00:05:32 -0700

    Add files via upload

---
+++ cs160-intro-fp/src/main/java/edu/sdsu/cs160l/functionalinterface/InterfaceVsLambda.java
@@ -0,0 +1,34 @@
+package edu.sdsu.cs160l.functionalinterface;
+
+public class InterfaceVsLambda {
+
+  /**
+   * Create an implementation of Operation interface
+   */
+  static class AddOperation implements Operation {
+    @Override
+    public int operate(int a, int b) {
+      return a + b;
+    }
+  }
+  
+  /*
+  private Operation add = (a, b) -> {
+	return a + b;  
+  };
+  */
+
+  /**
+   * Creating an instance of Operation class using lambda syntax
+   * (a, b) -> a + b
+   */
+  private static final Operation subtractOperation = (a, b) -> a - b;
+
+  public static void main(String[] args) {
+    Operation addOperation = new AddOperation();
+
+    System.out.println(addOperation.operate(10, 3));
+    System.out.println(subtractOperation.operate(10, 3));
+  }
+
+}
